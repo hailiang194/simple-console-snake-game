5d4b434c4297851e8ace55c7db39945ee3e8faf6
53k41iga <sonlaneedsme@outlook.com>
2018-02-20 08:21:51 +0700

    Update

--- logic-in-game.h
+++ logic-in-game.h
@@ -17,7 +17,8 @@ int is_snake_point(const object_t *snake, int x, int y)
 
 	while(i < MAX_LENGTH_SNAKE && snake[i].isExist)
 	{
-		if(snake[i].location.x == x && snake[i].location.y == y)
+		if(snake[i].location -> x == x 
+			&& snake[i].location -> y == y)
 			return TRUE;
 		i++;
 	}
@@ -28,14 +29,15 @@ int is_snake_point(const object_t *snake, int x, int y)
 
 int is_eat_fruit(const object_t *snake, location_t fruit)
 {
-	return ((snake[0].location.x == fruit.x) && (snake[0].location.y == fruit.y));
+	return ((snake[0].location -> x == fruit.x) 
+		&& (snake[0].location -> y == fruit.y));
 }
 
 
 int is_game_over(const object_t *snake)
 {
-	int headPointX = snake[0].location.x;
-	int headPointY = snake[0].location.y;
+	int headPointX = snake[0].location -> x;
+	int headPointY = snake[0].location -> y;
 
 	//snake is in block
 	if((headPointX == WIDTH || headPointX == -1) || (headPointY == HEIGHT || headPointY == -1))
@@ -50,7 +52,8 @@ int is_game_over(const object_t *snake)
 	//snake byte itself
 	while(i < MAX_LENGTH_SNAKE && snake[i].isExist)
 	{
-		if(headPointX == snake[i].location.x && headPointY == snake[i].location.y)
+		if(headPointX == snake[i].location -> x 
+			&& headPointY == snake[i].location -> y)
 			return TRUE;
 
 		i++;
